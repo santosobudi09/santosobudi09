2638c49bd465a65b40bce3377f904814e65bae20
santosobudi09 <113154508+santosobudi09@users.noreply.github.com>
2022-09-08 20:44:42 -0700

    UPRAK_BUDI_xtkj3
    nma : budi 
    kls : x  tkj 3

---
+++ budipython8.py
@@ -0,0 +1,11 @@
+list_kosong=[]
+list_string = ["Fikry","hastu","zafran","bayu"]
+#              0       1       2         3
+list_integer = [18, 24, 11, 21]
+list_mix = ["Fikry", 18, True, 52.0]
+
+print("Nama berikut", list_string[0],  "Tanggal lahir",  list_integer[0])
+print("Nama berikut", list_string[1],  "Tanggal lahir",  list_integer[1])
+print("Nama berikut", list_string[2],  "Tanggal lahir",  list_integer[2])
+print("Nama berikut", list_string[3],  "Tanggal lahir",  list_integer[3])
+
